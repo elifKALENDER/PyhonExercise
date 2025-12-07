0636bf257c1c6e4e65eb01acf795587a5af8a1c3
Elif kalender <elifkalender.tr@gmail.com>
2025-12-07 21:20:44 +0300

    added day05_meltdown_mitigation reactor project

---
+++ exercises/day01.py
@@ -0,0 +1,11 @@
+
+def hello():
+    return 'Hello, World!'
+hello()
+
+"""THIS SECTION PROVIDES SHORTCUTS AND USEFUL TIPS FOR PYTHON.
+     FEEL FREE TO REFER BACK TO THESE NOTES WHEN NEEDED.
+
+NOTE1: if you want to open explanation for a specific file you can use that code string:
+import exercises.day02_lasagna as d
+print(d.__doc__)"""
